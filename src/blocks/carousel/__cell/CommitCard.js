51ecdf24c179a31be2bcdbbff7f21449b2d94bc2
Alena Gorshkova <alenagorsh@gmail.com>
2020-01-20 19:29:16 +0300

    map-files deleted,bam-structure fixed

--- src/blocks/carousel/__cell/CommitCard.js
+++ src/blocks/carousel/__cell/CommitCard.js
@@ -1,33 +1,33 @@
-import dateFormat from '../../../../js/utils/dateFormat.js';
+import dateFormat from '../../../js/utils/dateFormat.js';
 
 export default class CommitCard {
     
 
     createCard(cardData) {
         const card = document.createElement('div');
-        card.classList.add('carousel-cell');
+        card.classList.add('carousel__cell');
 
         const cardImage = document.createElement('img');
-        cardImage.classList.add('carousel-cell__photo');
+        cardImage.classList.add('carousel__photo');
         cardImage.setAttribute('alt','Не удалось загрузить фотографию');
         cardImage.src = `${cardData.author.avatar_url}`;
     
         const infoColumn = document.createElement('div');
-        infoColumn.classList.add('carousel-cell__info');
+        infoColumn.classList.add('carousel__info');
 
         const cellColumn = document.createElement('div');
-        cellColumn.classList.add('carousel-cell__column');
+        cellColumn.classList.add('carousel__column');
 
         const commiterName = document.createElement('h3');
-        commiterName.classList.add('carousel-cell__name');
+        commiterName.classList.add('carousel__name');
         commiterName.textContent = cardData.commit.committer.name;
 
         const text = document.createElement('p');
-        text.classList.add('carousel-cell__message');
+        text.classList.add('carousel__message');
         text.textContent = cardData.commit.message;
 
         const commiterEmail = document.createElement('p');
-        commiterEmail.classList.add('carousel-cell__e-mail');
+        commiterEmail.classList.add('carousel__e-mail');
         commiterEmail.textContent = cardData.commit.committer.email;
 
         const cardDate = dateFormat(cardData.commit.author.date);
